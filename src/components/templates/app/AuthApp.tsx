eefeb7a6fb04fb9072af768397fedd45370c9997
Nandi Wong <nandi0315@gmail.com>
2021-03-13 17:28:04 +0800

    💫 feat: save token to local storage (#18)

--- src/components/templates/app/AuthApp.tsx
+++ src/components/templates/app/AuthApp.tsx
@@ -1,10 +1,11 @@
-import { useAppState } from 'components/hooks/ReduxStateHook';
+import { useAppState, useAuthState } from 'components/hooks/ReduxStateHook';
 import FullPageLoading from 'components/molecules/FullPageLoading';
 import AxiosInterceptor from 'libraries/axios';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { doInitBackgroundLoad } from 'states/auth/actions';
 import { useGRPCWrapper } from '../GRPCWrapper';
+import { setAuthState } from '../../../libraries/localstorage';
 
 /**
  * Will check the token from local storage and check if the user is logged in.
@@ -14,11 +15,19 @@ export default ({ children }: React.PropsWithChildren<{}>) => {
   const dispatch = useDispatch();
   const grpcWrapper = useGRPCWrapper();
   const { isInitialLoading } = useAppState();
+  const authState = useAuthState();
 
   useEffect(() => {
     // skip loading for maintenance mode
     dispatch(doInitBackgroundLoad(grpcWrapper));
   }, []);
+
+  // Store the authState once the token is updated
+  useEffect(() => {
+    if (authState.token) {
+      setAuthState(authState);
+    }
+  }, [authState.token]);
 
   return isInitialLoading ? (
     <FullPageLoading />
